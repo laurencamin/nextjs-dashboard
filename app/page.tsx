fb01ee6bc86c6c5e112a1ce144d332433241b643
Lauren <laurencc293@gmail.com>
2024-01-07 22:34:29 -0600

    six

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
   return (
     
@@ -21,6 +22,7 @@ export default function Page() {
           <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
+      
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
